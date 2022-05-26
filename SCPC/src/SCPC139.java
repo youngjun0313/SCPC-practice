18d032b06bc4928c67963c6c2093d8ef5622c668
youngjun0313 <zerojun0313@kaist.ac.kr>
2022-05-26 10:18:33 +0900

    SCPC139 complete-refactoring

--- SCPC/src/SCPC139.java
+++ SCPC/src/SCPC139.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 class SCPC139 {
     static String Answer;
 
-    public static void main(String args[]) throws Exception	{
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for(int test_case = 0; test_case < T; test_case++) {
+        for (int test_case = 0; test_case < T; test_case++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(stk.nextToken());
             int t = Integer.parseInt(stk.nextToken());
@@ -21,29 +20,29 @@ class SCPC139 {
             boolean[] must_zero = new boolean[n];
 
             for (int i = 0; i < n; i++) {
-                if(i-t>=0 && bits[i-t]=='0')
+                if (i - t >= 0 && bits[i - t] == '0')
                     must_zero[i] = true;
-                if(i+t<n && bits[i+t]=='0')
+                if (i + t < n && bits[i + t] == '0')
                     must_zero[i] = true;
             }
 
             for (int i = 0; i < n; i++) {
-                if(tempAns[i] != '1')
-                    tempAns[i]='0';
+                if (tempAns[i] != '1')
+                    tempAns[i] = '0';
 
-                if(bits[i]=='1') {
-                    if(i-t>=0 && tempAns[i-t]=='1')
+                if (bits[i] == '1') {
+                    if (i - t >= 0 && tempAns[i - t] == '1')
                         continue;
-                    else if(i+t<n && !must_zero[i+t])
-                        tempAns[i+t]='1';
+                    else if (i + t < n && !must_zero[i + t])
+                        tempAns[i + t] = '1';
                     else
-                        tempAns[i-t]='1';
+                        tempAns[i - t] = '1';
                 }
             }
 
-            Answer=String.valueOf(tempAns);
+            Answer = String.valueOf(tempAns);
 
-            System.out.println("Case #"+(test_case+1));
+            System.out.println("Case #" + (test_case + 1));
             System.out.println(Answer);
         }
     }
